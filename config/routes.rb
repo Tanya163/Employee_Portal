ca9e357e1e68e69a1ee444162a103ffe4d6e1646
Tanya163 <tanyataneja163@gmail.com>
2021-07-06 15:05:21 +0530

    routes for custom controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :employees
+  devise_for :employees, controllers: {
+    :sessions => "employees/sessions",
+    :registrations => "employees/registrations" }
   root to: "pages#index"
   get '/contact', to: 'pages#contact'
 end
